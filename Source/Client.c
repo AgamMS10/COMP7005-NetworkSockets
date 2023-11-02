d5cef046214a45678d44cf30786b0ca54a1025aa
AgamMS10 <44703146+AgamMS10@users.noreply.github.com>
2023-11-02 10:58:45 -0700

    IPv6 FileName Function

--- Source/Client.c
+++ Source/Client.c
@@ -6,26 +6,26 @@
 #include <sys/un.h>
 #include <unistd.h>
 
-#define MAX_IP_LENGTH 16
+#define MAX_IP_LENGTH 46
 #define MAX_PORT_LENGTH 6
 
-static int socket_create(void);
+
+static int socket_create(int family);
 static int connect_to_server(const char *ip, int port);
 static void socket_close(int sockfd);
 static void read_file(const char *file_path, char **file_data, size_t *file_size);
-static void send_file_data(int server_fd, const char *filename, const char *file_data, size_t file_size);
+static void send_file_metadata(int server_fd, const char *filename, size_t file_size);
+static void send_file_content(int server_fd, const char *file_data, size_t file_size);
 static char *parse_filename(const char *file_path);
+static int determine_address_family(const char *ip);
 
 int main(int argc, char *argv[]) {
     if (argc < 4) {
-        printf("Usage: %s <IPv4> <port> <file1> [<file2> ...]\n", argv[0]);
+        printf("Usage: %s <IPv4/IPv6> <port> <file1> [<file2> ...]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-
-    
     const char *server_ip = argv[1];
     int server_port = atoi(argv[2]);
-
     int sockfd = connect_to_server(server_ip, server_port);
 
     for (int i = 3; i < argc; i++) {
@@ -34,17 +34,25 @@ int main(int argc, char *argv[]) {
         read_file(argv[i], &file_data, &file_size);
 
         char *filename = parse_filename(argv[i]);
-        send_file_data(sockfd, filename, file_data, file_size);
+        send_file_metadata(sockfd, filename, file_size);
+        send_file_content(sockfd, file_data, file_size);
 
         free(file_data);
         free(filename);
     }
 
     socket_close(sockfd);
-
     return EXIT_SUCCESS;
 }
 
+static int socket_create(int family) {
+    int sockfd = socket(family, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    return sockfd;
+}
 
 static void read_file(const char *file_path, char **file_data, size_t *file_size) {
     FILE *file = fopen(file_path, "rb");
@@ -76,16 +84,17 @@ static void read_file(const char *file_path, char **file_data, size_t *file_size
     fclose(file);
 }
 
-static void send_file_data(int server_fd, const char *filename, const char *file_data, size_t file_size) {
+static void send_file_metadata(int server_fd, const char *filename, size_t file_size) {
     uint8_t filename_size = strlen(filename);
 
     if (send(server_fd, &filename_size, sizeof(uint8_t), 0) == -1 ||
         send(server_fd, filename, filename_size, 0) == -1 ||
         send(server_fd, &file_size, sizeof(uint32_t), 0) == -1) {
         perror("Error sending file metadata");
-        return;
     }
+}
 
+static void send_file_content(int server_fd, const char *file_data, size_t file_size) {
     size_t remaining = file_size;
     const char *data_ptr = file_data;
 
@@ -101,11 +110,11 @@ static void send_file_data(int server_fd, const char *filename, const char *file
 
         remaining -= bytes_sent;
         data_ptr += bytes_sent;
-        //Add Usleep for testing purposes
-        //usleep(100000);
+        // Add Usleep for testing purposes
+        // usleep(100000);
     }
 
-    printf("Sent file: %s\n", filename);
+    printf("Sent file content.\n");
 }
 
 static char *parse_filename(const char *file_path) {
@@ -124,38 +133,56 @@ static char *parse_filename(const char *file_path) {
     return result;
 }
 
-static int socket_create(void) {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        perror("Socket creation failed");
+static int connect_to_server(const char *ip, int port) {
+    int family = determine_address_family(ip);
+    if (family == -1) {
+        fprintf(stderr, "Invalid IP address format.\n");
         exit(EXIT_FAILURE);
     }
 
+    int sockfd = socket_create(family);
+
+    if (family == AF_INET) {
+        struct sockaddr_in server_addr;
+        memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        inet_pton(AF_INET, ip, &(server_addr.sin_addr));
+        server_addr.sin_port = htons(port);
+
+        if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+            perror("Connect failed. Make sure the server is running and the IP address and port are correct.");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        struct sockaddr_in6 server_addr6;
+        memset(&server_addr6, 0, sizeof(server_addr6));
+        server_addr6.sin6_family = AF_INET6;
+        inet_pton(AF_INET6, ip, &(server_addr6.sin6_addr));
+        server_addr6.sin6_port = htons(port);
+
+        if (connect(sockfd, (struct sockaddr *)&server_addr6, sizeof(server_addr6)) == -1) {
+            perror("Connect failed. Make sure the server is running and the IP address and port are correct.");
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    printf("Connected to the server.\n");
     return sockfd;
 }
 
-
-static int connect_to_server(const char *ip, int port) {
-    int sockfd = socket_create();
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
-    server_addr.sin_port = htons(port);
-
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Connect failed. Make sure the server is running and the IP address and port are correct.");
-        exit(EXIT_FAILURE);
+static int determine_address_family(const char *ip) {
+    struct in6_addr buf;
+    if (inet_pton(AF_INET, ip, &buf)) {
+        return AF_INET;
+    } else if (inet_pton(AF_INET6, ip, &buf)) {
+        return AF_INET6;
     } else {
-        printf("Connected to the server.\n");
+        return -1; 
     }
-
-    return sockfd;
 }
 
 static void socket_close(int sockfd) {
     if (close(sockfd) == -1) {
         perror("Error closing socket");
-        exit(EXIT_FAILURE);
     }
 }
